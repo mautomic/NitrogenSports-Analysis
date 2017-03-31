79fd235c8a86e0f36c8a56e06e3f29e3b249cba8
maurya373 <maurya373@gmail.com>
2017-03-31 18:38:35 -0500

    Finished initial KellyCrit calculator

--- KellyCrit.py
+++ KellyCrit.py
@@ -1,8 +1,25 @@
 
 # Use the Kelly Criterion to figure out optimal bet
 
-# adjust these vars
-bankroll = 0.09024
-betOdds = 1.863
+# TODO: How can we predict probability of the pick without 
+#       relying on the odds?
+
+# values in BTC
+bankroll = 0.087245
+betOdds = 1.948
+probSuccess = 1/(betOdds)
+probFailure = 1.0-probSuccess
+
+fraction = ((betOdds * probSuccess) - probFailure)/betOdds
+
+print ("Success% : " + str(probSuccess))
+
+if (bankroll * fraction) < 0:
+  print ("Don't Bet")
+else:
+  print ("Bet Size : " + str(bankroll * fraction))
+
+
+
 
 
