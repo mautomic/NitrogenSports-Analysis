42880b9f624ea03696cd062e605fc0978ae1c5e3
maurya373 <maurya373@gmail.com>
2017-04-01 02:26:53 -0500

    now can keep track of wins and losses for NBA

--- Analysis.py
+++ Analysis.py
@@ -25,63 +25,97 @@
 # Program starts here
 def main():
 
-  # get data organized in list
-  betslips = getData()
+	# get data organized in list
+	betslips = getData()
 
-  # separate data into CloudBet and NitrogenSports
-  cloudBets = []
-  nitroBets = []
+	# separate data into CloudBet and NitrogenSports
+	cloudBets = []
+	nitroBets = []
 
-  for bet in betslips:
-    if (bet[0] == "WIN" or bet[0] == "LOSS" or bet[0] == "PUSH"):
-      cloudBets.append(bet)
-    else:
-      nitroBets.append(bet)
+	for bet in betslips:
+		if (bet[0] == "WIN" or bet[0] == "LOSS" or bet[0] == "PUSH"):
+			cloudBets.append(bet)
+		else:
+			nitroBets.append(bet)
+
+	getRecord("NBA", "All", cloudBets, nitroBets)
 
 
 def getRecord(sport, betType, cloudList, nitroList):
 
-  wins = 0
-  losses = 0
-  netProfit = 0
+	wins = 0
+	losses = 0
+	pushes = 0
+	netProfit = 0
 
-  if (betType == "ML"):
-    # ML Analysis
-    print ('a')
+	if betType == "All":
 
-  elif (betType == "Spread"):
-    # Spread Analysis
-    print ('a')
+		for cloud in cloudList:
 
-  elif (betType == "OverUnder"):
-    # OverUnder Analysis
-    print ('a')
+			print cloud
+			if cloud[0] == "WIN":
+				wins = wins + 1
+			if cloud[0] == "LOSS":
+				losses = losses + 1
+			if cloud[0] == "PUSH":
+				pushes = pushes + 1
 
-  elif (betType == "PlayerProps"):
-    # Prop Analysis
-    print ('a')
+		for nitro in nitroList:
+		 	
+		 	if nitro[7] == sport or nitro[7] == "NBA (LIVE)":
+
+		 		print nitro
+				if nitro[5] == "win":
+					wins = wins + 1
+				if nitro[5] == "lose":
+					losses = losses + 1
+				if nitro[5] == "push":
+					pushes = pushes + 1
+
+  	elif (betType == "ML"):
+  		print ('a')
+
+  	elif (betType == "Spread"):
+	 	print ('a')
+
+  	elif (betType == "OverUnder"):
+	 	print ('a')
+
+  	elif (betType == "PlayerProps"):
+	 	print ('a')
+
+	print (str(wins) + "-" + str(losses) + "-" + str(pushes))
 
 
 # Function to open file of data and parse it into a list of betslips
 def getData():
 
-  f = open('NS Wagers 3-31-17.txt', 'r')
+	f = open('NS Wagers 3-31-17.txt', 'r')
 
-  betslips = []
-  temp = []
+	betslips = []
+	temp = []
+	count = 0
 
-  for line in f:
-  	line = line.strip('\n')
+	for line in f:
+		line = line.strip('\n')
+	 
+		# break to start after Super Bowl 51, continue for prior
+		if "PHASE" in line:
+			break
 
-  	if len(line) is not 0:
-  		temp.append(line)
-  	else:
-  		betslips.append(temp)
-  		temp = []
+		# Parse out Blank lines and Parlays
+		if len(line) is not 0:
+			temp.append(line)
+			count = count + 1
+		else:
+			if (count < 16):
+				betslips.append(temp)
+			temp = []
+			count = 0
 
-  # to remove empty lists (spaces)
-  bets = [x for x in betslips if x]
-  return bets
+	# to remove empty lists (spaces)
+	bets = [x for x in betslips if x]
+	return bets
 
 
 # TODO
@@ -91,4 +125,4 @@ def getData():
 # Win/lose percentages
  
 if __name__ == '__main__':
-  main()
+	main()
